8651be745fd7358a1343b6a4c4de567257d1fea0
Derek Lin <derek.lin@blackrock.com>
2018-04-10 16:40:20 -0700

    followed example

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -3,5 +3,4 @@ export * from './e2e-check';
 export * from './in-memory-data.service';
 export * from './master-detail-commands';
 export * from './model';
-export * from './reactive-data.service';
 export * from './toast.service';
